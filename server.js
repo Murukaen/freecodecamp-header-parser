df012cf1851e62b55f60ded0ad5545b29cc52ed2
Razvan <razzvan.savu@gmail.com>
2017-04-26 07:58:03 +0000

    Fix regexp

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT
-const languageRegEx = /^([^,]*),.*$/
+const languageRegEx = /^([^,]*),?.*$/
 const softwareRegEx = /.*\((.*;[^\(]*)\).*/
 
 app.get('/', function (req, res) {
