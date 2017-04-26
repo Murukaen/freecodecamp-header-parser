9fc3495adc2fd22a47051c01ab4dcdc40b21a85e
Razvan <razzvan.savu@gmail.com>
2017-04-26 07:39:25 +0000

    Add /api/whoami route

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var express = require('express')
 var app = express()
 const port = process.env.PORT
+const languageRegEx = /^([^,]*),.*$/
+const softwareRegEx = /.*\((.*;[^\(]*)\).*/
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('It works!')
+})
+
+app.get('/api/whoami', (req, res) => {
+  res.send({
+    'ipaddress': req.header('x-forwarded-for'),
+    'language': languageRegEx.exec(req.header('accept-language'))[1],
+    'software': softwareRegEx.exec(req.header('user-agent'))[1]
+  })
 })
 
 app.listen(port, function () {
